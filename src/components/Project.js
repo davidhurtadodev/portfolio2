26b96307124f041c6383134eb6d535e4a614bc5b
David Hurtado <davidhuertado@gmail.com>
2022-02-18 19:36:43 -0300

    feat figures animation, projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import '../styles/Project.css';
 
-const Project = ({ img, title, info }) => {
+const Project = ({ img, title, info, projectClass, figure }) => {
   return (
-    <div className="project">
-      <div className="img-container">
-        <img src={img} alt="title" />
-      </div>
-      <div className="text-container">
-        <h4>{title}</h4>
-        <p>{info}</p>
+    <div className="project-wrapper">
+      <span className={figure}></span>
+      <div className={`project ${projectClass}`}>
+        <div className="img-container">
+          <img src={img} alt="title" />
+        </div>
+        <div className="text-container">
+          <h4>{title}</h4>
+          <p>{info}</p>
+        </div>
+        <div className="btn-container">
+          <button type="button" className="demo">
+            Demo
+          </button>
+          <button type="button" className="code">
+            <i class="fa-brands fa-github pointer"></i>Code
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,40 +5,59 @@ import splitterMobile from '../assets/splitter-mobile.png';
 import pokemonMobile from '../assets/pokemon-mobile.png';
 import timeTrackerMobile from '../assets/time-tracker-mobile.png';
 import shorteningImg from '../assets/shortening.png';
+import trackerLaptop from '../assets/time-tracker-laptop.png';
 
 const projectsData = [
+  {
+    title: 'Time Tracking Dashboard',
+    info: 'Tracking Dashboard made with React, class components and states',
+    img: trackerLaptop,
+    url: '',
+    projectClass: 'time-tracking',
+    figure: 'triangle',
+  },
   {
     title: 'Shortly',
     info: 'Landing page of an app that allows to shortening URL. Made with React Hooks, sessionStorage and REST APi',
     img: shorteningImg,
     url: '',
+    projectClass: 'shortly',
+    figure: 'circle',
   },
   {
     title: 'Splitter',
     info: 'App that allows to calculate tips. Made with React',
     img: splitterMobile,
     url: '',
-  },
-  {
-    title: 'Time Tracking Dashboard',
-    info: 'Tracking Dashboard made with React, class components and states',
-    img: timeTrackerMobile,
-    url: '',
+    projectClass: 'splitter',
+    figure: 'square',
   },
   {
     title: 'Pokemon Memory Game',
     info: 'Memory game made with React with use of REST API',
     img: pokemonMobile,
     url: '',
+    projectClass: 'pokemon',
+    figure: 'circle',
   },
 ];
 
 const Projects = () => {
-  const renderedProjects = projectsData.map(({ title, img, info, url }) => {
-    return (
-      <Project title={title} img={img} info={info} url={url} key={title} />
-    );
-  });
+  const renderedProjects = projectsData.map(
+    ({ title, img, info, url, projectClass, figure }) => {
+      return (
+        <Project
+          title={title}
+          img={img}
+          projectClass={projectClass}
+          info={info}
+          url={url}
+          key={title}
+          figure={figure}
+        />
+      );
+    }
+  );
   return (
     <section className="projects">
       <h2>
